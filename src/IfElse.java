357cf007bf9f0531b6deedfbe70b7c0053a009e9
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-02-27 19:11:38 +0100

    add code in ifelse

--- src/IfElse.java
+++ src/IfElse.java
@@ -6,9 +6,14 @@ public class IfElse {
        int numero1 = 1;
         int numero2 = 5;
         int numero3 = 20;
+        int numero4 = 222;
 
-        if (numero1< numero3){
+        if (numero1> numero3){
             System.out.println("verdadero");
+        }else {
+            System.out.println("falso");
         }
+        System.out.println("fin");
+
     }
 }
