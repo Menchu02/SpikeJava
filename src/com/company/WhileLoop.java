743891a358358f88fd20bb35d1ed5bd30ae2d2a9
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-02-27 19:29:54 +0100

    WhileLopp

---
+++ src/com/company/WhileLoop.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+
+        int count = 0;
+
+        while ( count<10){
+            System.out.println("Hola mundo " + count);
+            count++;
+
+
+
+        }
+        System.out.println("fin");
+    }
+
+}
