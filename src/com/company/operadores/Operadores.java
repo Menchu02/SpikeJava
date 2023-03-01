6aad1f5d4ecf17ad7ad1e85b2d4fb694207d9588
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-03-01 12:11:03 +0100

    Operadores

--- src/com/company/operadores/Operadores.java
+++ src/com/company/operadores/Operadores.java
@@ -1,4 +1,22 @@
 package com.company.operadores;
 
 public class Operadores {
+    public static void main(String[] args) {
+        //aritmeticos
+
+        int numero1 = 10;
+        int numero2 = 20;
+        int resultadoSuma = numero2 + numero1;
+
+        System.out.println(resultadoSuma);
+        System.out.println(resultadoSuma + 5.77);
+
+        int resultadoResta = numero1 - numero2;
+        System.out.println(resultadoResta);
+
+        //Comparaciones
+
+        
+
+    }
 }
