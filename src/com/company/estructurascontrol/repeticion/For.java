ba67183ada07e0d8ffcdb09deccb9ff2a951f5dc
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-03-02 12:03:14 +0100

    For

---
+++ src/com/company/estructurascontrol/repeticion/For.java
@@ -0,0 +1,14 @@
+package com.company.estructurascontrol.repeticion;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0;  i <10; i++ ){
+            System.out.println("Hola amigos, soy el  " + i);
+        }
+        String [] nombres = {"pepe","laura","silvia"};
+
+        for (int i = 0;i < nombres.length; i++){
+            System.out.println("Mi nombre es: " + nombres[i]);
+        }
+    }
+}
